e155a5fdcc8e0c00ee7f75f9d10bc7b1ce1a0485
pwa4owski <312431@niuitmo.ru>
2024-11-06 14:52:43 +0300

    add all entities, test create-event

--- baron/models/models.py
+++ baron/models/models.py
@@ -1,12 +1,10 @@
 from peewee import (
-    AutoField,
     BooleanField,
     CharField,
-    DateField,
     ForeignKeyField,
     IntegerField,
     Model,
-    PostgresqlDatabase, TimestampField, Check,
+    PostgresqlDatabase, TimestampField, Check, DateTimeField, CompositeKey,
 )
 
 db = PostgresqlDatabase(
@@ -30,8 +28,8 @@ class User(Model):
         table_name = 'users'
 
 class Event(Model):
-    id = IntegerField()
-    user = ForeignKeyField(User, backref="events")
+    id = IntegerField(primary_key=True)
+    user = ForeignKeyField(User, backref="events", column_name="author_id")
     created_at = TimestampField(null=True)
     min_person_cnt = IntegerField(default=0, constraints=[Check('min_person_cnt >= 0')])
     status_id = CharField(null=True)
@@ -41,7 +39,41 @@ class Event(Model):
         schema = 'pwa4owski_test'
         table_name = 'events'
 
+class UserEvent(Model):
+    user = ForeignKeyField(User, backref='events', on_delete='CASCADE')
+    event = ForeignKeyField(Event, backref='users', on_delete='CASCADE')
+    joined_at = TimestampField()
+
+    class Meta:
+        database = db
+        schema = 'pwa4owski_test'
+        table_name = 'event_group'
+        primary_key = CompositeKey('user', 'event')
 
 
-db.bind([User, Event], bind_refs=False, bind_backrefs=False)
+class Option(Model):
+    id = IntegerField(primary_key=True)
+    event = ForeignKeyField(Event, backref="options", on_delete='CASCADE')
+    start_time = DateTimeField()
+    place_name = CharField()
+    place_link = CharField(null=True)
+    author = ForeignKeyField(User, backref='authored_options', on_delete='CASCADE')
+
+    class Meta:
+        database = db
+        table_name = 'options'
+
+
+class UsersOptions(Model):
+    user = ForeignKeyField(User, backref='user_options', on_delete='CASCADE')
+    option = ForeignKeyField(Option, backref='option_users', on_delete='CASCADE')
+    match = BooleanField()
+
+    class Meta:
+        database = db
+        table_name = 'users_options'
+        primary_key = CompositeKey('user', 'option')
+
+
+db.bind([User, Event, UserEvent, Option, UsersOptions], bind_refs=False, bind_backrefs=False)
 
