6ef5200623a55b20236dd23e0e668d8a43375407
fatalem0 <madchaterrer@gmail.com>
2024-11-06 21:06:51 +0300

    feat: divide cmds

--- baron/commands/create_event_cmd.py
+++ baron/commands/create_event_cmd.py
@@ -1,14 +1,11 @@
 import logging
 
-from peewee import IntegrityError
-from telegram import Update, constants
+from telegram import constants, Update
 from telegram.constants import ParseMode
-from telegram.ext import Application, CommandHandler, ConversationHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import ConversationHandler, ContextTypes
 
-from baron.events import create_event_cmd
-from baron.models import db, Users
+from baron.events import create_event
 from baron.users import find_user_by_id
-from configs.models import Config
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -21,40 +18,15 @@ logger = logging.getLogger(__name__)
 DATE, PLACE, ATTENDEES, MIN_ATTENDEES, FINISH_CREATE_EVENT = range(5)
 
 
-async def _start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    username = update.effective_user.username
-    user_id = update.effective_user.id
-
-    logger.info(f"Received /start from user: {username} with ID: {user_id}")
-
-    if db.is_closed():
-        db.connect()
-
-    try:
-        user, created = Users.get_or_create(id=user_id, defaults={'username': username})
-        if created:
-            await update.message.reply_text(f"Welcome, {username}! You have been registered.")
-            logger.info(f"New user registered: {username}")
-        else:
-            await update.message.reply_text(f"Welcome back, {username}!")
-            logger.info(f"Returning user: {username}")
-    except IntegrityError as e:
-        logger.error(f"Error inserting user {username}: {e}")
-        await update.message.reply_text("An error occurred while registering. Please try again.")
-    finally:
-        if not db.is_closed():
-            db.close()
-
-
-async def _create_event_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def create_event_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Захотели собраться? А что за повод-то?")
 
-    context.chat_data['is_group'] = update.message.chat.type == constants.ChatType.PRIVATE
+    context.chat_data['is_group'] = update.message.chat.type != 'private'
 
     return DATE
 
 
-async def _set_date(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def set_date(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.chat_data['event_name'] = update.message.text
 
     logger.info(
@@ -70,7 +42,7 @@ async def _set_date(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return PLACE
 
 
-async def _set_place(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def set_place(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.chat_data['event_date'] = update.message.text
 
     logger.info(
@@ -84,7 +56,7 @@ async def _set_place(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "Где соберётесь?"
     )
 
-    if context.chat_data.get('is_group', False):
+    if context.chat_data['is_group']:
         chat_id = update.message.chat_id
         chat_members = await context.bot.getChatMember(chat_id)
         chat_members_usernames = [member.user.username for member in chat_members]
@@ -94,7 +66,7 @@ async def _set_place(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return ATTENDEES
 
 
-async def _opt_set_attendees(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def opt_set_attendees(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.chat_data['event_place'] = update.message.text
 
     logger.info(
@@ -111,7 +83,7 @@ async def _opt_set_attendees(update: Update, context: ContextTypes.DEFAULT_TYPE)
     return MIN_ATTENDEES
 
 
-async def _set_min_attendees(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def set_min_attendees(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.chat_data.get('event_attendees', False):
         context.chat_data['event_attendees'] = update.message.text.strip().split('\n')
 
@@ -129,7 +101,7 @@ async def _set_min_attendees(update: Update, context: ContextTypes.DEFAULT_TYPE)
     return FINISH_CREATE_EVENT
 
 
-async def _finish_create_event(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def finish_create_event(update: Update, context: ContextTypes.DEFAULT_TYPE):
     event_author_name = update.message.from_user.username
     user_id = update.effective_user.id
 
@@ -140,7 +112,7 @@ async def _finish_create_event(update: Update, context: ContextTypes.DEFAULT_TYP
     event_attendees = context.chat_data['event_attendees'],
     event_min_attendees = update.message.text
 
-    new_event_id = create_event_cmd(
+    new_event_id = create_event(
         event_author_id,
         event_name,
         event_date,
@@ -183,40 +155,3 @@ async def _finish_create_event(update: Update, context: ContextTypes.DEFAULT_TYP
     await update.message.reply_text(finish_msg, parse_mode=ParseMode.MARKDOWN)
 
     return ConversationHandler.END
-
-
-async def _help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message = (
-        "BarON бот\n"
-        "---------\n"
-        "Команды:"
-        "*/create_event:* Создать мероприятие\n"
-        "*/help:* Вывести все команды\n"
-    )
-
-    await update.message.reply_text(message, parse_mode=ParseMode.MARKDOWN)
-
-
-def main(config: Config) -> None:
-    application = Application.builder().token(config.telegram_token).build()
-
-    handles = [
-        CommandHandler("start", _start),
-        ConversationHandler(
-            entry_points=[CommandHandler("create_event", _create_event_command)],
-            states={
-                DATE: [MessageHandler(filters.TEXT, _set_date)],
-                PLACE: [MessageHandler(filters.TEXT, _set_place)],
-                ATTENDEES: [MessageHandler(filters.TEXT, _opt_set_attendees)],
-                MIN_ATTENDEES: [MessageHandler(filters.TEXT, _set_min_attendees)],
-                FINISH_CREATE_EVENT: [MessageHandler(filters.TEXT, _finish_create_event)]
-            },
-            fallbacks=[]
-        ),
-        CommandHandler("help", _help_command)
-    ]
-
-    for handle in handles:
-        application.add_handler(handle)
-
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
--- baron/events.py
+++ baron/events.py
@@ -3,7 +3,7 @@ from peewee import IntegrityError
 from baron.models import Events, db, EventOptions, UsersEvents
 
 
-def create_event_cmd(event_author_id, event_name, event_date, event_place, event_attendees, event_min_attendees):
+def create_event(event_author_id, event_name, event_date, event_place, event_attendees, event_min_attendees):
     try:
         with db.atomic():
             new_event = Events.create(
